6b2dc5aa5f9c10b8da88d532928d37099baa8037
HericlysCyber <josehericlys939@gmail.com>
2026-09-24 00:03:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.RIVER_CONFIG={
  supabaseKey:"sb_publishable_A87wjQ82Uw6_CCmKg9TsQw_ruSBqSPF",
  radioUrl:"https://hericlyscyber.github.io/River-Indor/",
  clientFunction:"admin-client",
- cartazeamentoBackendUrl:"http://127.0.0.1:5000",
+ cartazeamentoBackendUrl:"https://river-cartazeamento.onrender.com/",
  riverMusicBackendUrl:"http://127.0.0.1:3000"
 };
